961ab802f4e2be2e5b9dfca5330c51b6c50384a9
lukker444 <zp.bogdan.osnova@gmail.com>
2025-05-13 18:14:44 +0200

    Creating id counter + list from file

--- main.py
+++ main.py
@@ -5,30 +5,53 @@ def manager():
     print("To delete an exersise: 4")
     print("To quit the program:   0")
 
-id = 0
-def dict_exersise(name, description, priority, status):
-   dict_exersise = {}
-   dict_exersise["id"] = id
-   dict_exersise["name"] = name
-   dict_exersise["description"] = description
-   dict_exersise["priority"] = priority
-   dict_exersise["status"] = status
-   return dict_exersise
+id = 1
 
+def creating_list_from_file():
+    try:
+        second_append_list = []
+        helper_list = []
+        final_list = []
+        with open("list.txt", "r") as file:
+            first_append_list = file.read().split("|")
+        for i in first_append_list:
+            k = i.split("/")
+            second_append_list.append(k)
+        for i in second_append_list:
+            for x in i:
+                a = x.split("-")
+                helper_list.append(tuple(a))
+            w = dict(helper_list)  
+            final_list.append(w)
+        return  final_list
+    except(ValueError):
+        print("There aren't any exersises :(")
 
-
+def id_counter():
+    try:
+        last_exersise = list1[-1:]
+        for i in last_exersise:
+            id = int(i["id"]) + 1
+        return id
+    except(TypeError):
+        return 1
+    
 def saving_exersises_to_file():
+    with open("list.txt", "r") as file:
+        reading_file = file.read()
     with open("list.txt", "a") as file:
-        file.write(f"{str(dict_exersise)}\n")
-
+        if reading_file.startswith("i"):
+            file.write(f"|id-{id}/name-{name}/description-{description}/priority-{priority}/status-{status}")
+        else:
+            file.write(f"id-{id}/name-{name}/description-{description}/priority-{priority}/status-{status}")
+   
 def create_exercise():  
     name = input("Name: ")
-    description = input("Description ")
+    description = input("Description: ")
     priority = input("Priority low/mid/high: ")
     status = input("Status new/processing/end: ")
     return name, description, priority, status
     
-
 starting_code = input("To start anything enter start: ")
 if starting_code == "start":
     while True:
@@ -38,7 +61,8 @@ if starting_code == "start":
             break
         elif choosing_number == "1":
             name, description, priority, status = create_exercise()
-            dict_exersise = dict_exersise(name, description, priority, status)
+            list1 = creating_list_from_file()
+            id = id_counter()
             saving_exersises_to_file()
             
         
